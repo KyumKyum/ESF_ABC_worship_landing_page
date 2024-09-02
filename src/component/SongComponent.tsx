00b15ad26d39edea2d5a196b420a7806a186fb73
Kyu Min Lim <mycheesepasta@gmail.com>
2024-09-02 09:47:21 +0900

    fix: fixed margin value

--- src/component/SongComponent.tsx
+++ src/component/SongComponent.tsx
@@ -6,13 +6,13 @@ function SongComponent() {
   return (
     <div className="relative w-full">
       <p className="font-snow text-white text-center text-xl">Songs</p>
-      <div className="flex flex-col w-full justify-center items-start mt-10">
+      <div className="flex flex-col w-full justify-center items-center mt-10">
         <p className="mt-1 text-sm font-snow text-white">1. O Come to the Altar</p>
         <p className="mt-1 text-sm font-snow text-white">2. Our God (Awesome God)</p>
         <p className="mt-1 text-sm font-snow text-white">3. Way Maker</p>
         <p className="mt-1 text-sm font-snow text-white">4. One Way (Amazing Grace)</p>
       </div>
-      <div className="flex flex-col w-full justify-center items-end mt-10">
+      <div className="flex flex-col w-full justify-center items-center mt-10">
         <p className="mt-1 text-sm font-snow text-white">5. Rest on Us (Set a Fire)</p>
         <p className="mt-1 text-sm font-snow text-white">6. Battle Belongs</p>
         <p className="mt-1 text-sm font-snow text-white">7. Nothing is Impossible (Take it All)</p>
